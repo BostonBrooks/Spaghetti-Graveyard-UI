291be094d3376ea20eeabf2bfc1293b0c5228ea9
Boston <pbrooks193@gmail.com>
2024-03-06 18:49:35 +1000

    working out classes as we go, implementing main.c

--- manual/main.c
+++ manual/main.c
@@ -8,22 +8,32 @@ int main (void){
 	char game_name[] = "game";
 
 	//create bbGame from gamedata.txt
-	char path_to_gamedata[256];
-	sprintf(path_to_gamedata, "../games/%s/gamedata.txt", game_name);
-	bbGame* game = bbGame_new(path_to_gamedata);
+	char gamedata_path[256];
+	sprintf(gamedata_path, "../games/%s/gamedata.txt", game_name);
+	bbGame* game = bbGame_new(gamedata_path);
 	g_Game = game;
 
-	char map_name = g_Game->m_MapNames[0];
-	char path_to_mapdata[256];
-	sprintf(path_to_gamedata, "../games/%s/maps/%s/mapdata.txt", game, map_name);
-	bbMap* map0 = bbMap_new(path_to_mapdata);
-	g_Game->m_Maps[0] = map0;
+	char* map_name = g_Game->m_MapNames[0];
+	char file_path[256];
 
-	char textures_file
+    sprintf(file_path, "../games/%s/maps/%s/mapdata.txt", game, map_name);
+    bbMap* map0 = bbMap_new(file_path);
+    g_Game->m_Maps[0] = map0;
 
+    sprintf(file_path, "../games/%s/maps/%s/textures.csv", game, map_name);
+    bbTextures* textures = bbTextures_new(file_path);
+    g_Game->m_Maps[0]->m_Textures = textures;
 
+    sprintf(file_path, "../games/%s/maps/%s/sprites.csv", game, map_name);
+    bbSprites* sprites = bbSprites_new(file_path, textures);
+    g_Game->m_Maps[0]->m_Sprites = sprites;
 
+    sprintf(file_path, "../games/%s/maps/%s/animations.csv", game, map_name);
+    bbAnimations* animations = bbAnimations_new(file_path, sprites);
+    g_Game->m_Maps[0]->m_Animations = animations;
 
+    sprintf(file_path, "../games/%s/maps/%s/skins.csv", game, map_name);
+    bbSkins* skins = bbSkins_new(file_path, animations);
+    g_Game->m_Maps[0]->m_Skins = skins;
 
-
-}
+    // draw one frame of an animation to the window
