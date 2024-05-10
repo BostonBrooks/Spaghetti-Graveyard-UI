354cee997f88281f6ed943e7032882850c3404c6
Boston Brooks <pbrooks193@gmail.com>
2024-05-10 18:38:15 +1000

    manually rolling back main

---
+++ games/game/maps/demo/widgets/decal.h
@@ -0,0 +1,75 @@
+/**
+ * @file
+ * @brief The functions in this folder define the behavior of an object with class bbWidget and type NULL
+ */
+#define BBDEBUG
+
+#include "headers/bbPrintf.h"
+
+#include "headers/bbGeometry.h"
+#include "headers/bbWidget.h"
+#include "headers/bbGame.h"
+#include "headers/bbMap.h"
+#include "headers/bbWidgetTimer.h"
+
+///	Spawn a null widget on selected map at coordinates mc
+I32 bbWidgetNew_Decal(bbWidget** reference, bbWidgets* widgets, bbScreenCoordsI sc, bbWidget* parent){
+
+
+
+    bbWidget* widget;
+    bbWidget_empty_new(&widget, widgets, sc, NULL);
+
+    bbPrintf("widget -> map = &d\n", widget->p_Node.p_Pool.Map);
+
+    I32 flag;
+    widget->m_String = "Decal / Root Widget";
+
+    bbWidgetFunctions* functions = widgets->m_Functions;
+    widget->v_OnMouse = bbWidgetFunctions_getInt(functions, f_WidgetMouseHandler, "clickDecal");
+
+
+    bbScreenCoordsF SCF;
+    bbScreenCoordsI SCI;
+
+    SCF.x = 0;
+    SCF.y = 0;
+    SCI = bbScreenCoordsF_getI(SCF, &g_Game->m_Maps[widget->p_Node.p_Pool.Map]->p_Constants);
+    widget->m_ScreenCoords = SCI;
+
+    SCF.x = 1280;
+    SCF.y = 720;
+    SCI = bbScreenCoordsF_getI(SCF, &g_Game->m_Maps[widget->p_Node.p_Pool.Map]->p_Constants);
+    widget->m_Dimensions = SCI;
+
+
+    widget->m_AnimationInt[0] = 162; //DECAL_1280
+    widget->v_DrawFunction[0] = bbWidgetFunctions_getInt(functions, f_WidgetDrawFunction, "sprite");
+
+
+    *reference = widget;
+    return f_Success;
+}
+
+/// Update widget
+I32 bbWidget_Decal_update(bbWidget* widget){}
+
+/// Send a command to the widget / update widget, etc
+I32 bbWidget_Decal_onCommand(bbWidget* widget, void* command){}
+
+/// Delete widget
+I32 bbWidget_Decal_delete(bbWidget* widget){}
+
+/// Draw widget to screen
+I32 bbWidget_Decal_draw(bbWidget* widget){}
+
+
+/// if you click the decal, nothing happens
+I32 bbWidgetClick_Decal(void* void_mouseEvent, void* void_widget){
+    bbMouseEvent* event = void_mouseEvent;
+    if (event->m_type ==  f_MouseLeft) {
+        bbPrintf("You clicked nothing\n");
+    }
+    return f_Break;
+
+}
