a8bbbdc9acd9d5ceaba5382c5b0de4eb623ce389
Boston <pbrooks193@gmail.com>
2024-08-16 18:59:46 +1000

    playing around with file system

---
+++ engine/algorithms/bbIntTypes.h
@@ -0,0 +1,13 @@
+/**
+ * @brief This header file contains wrappers for int types used in the program
+ *
+*/
+
+#ifndef BBINTTYPES
+#define BBINTTYPES
+
+#include <inttypes.h>
+#define I32 int32_t
+#define U32 uint32_t
+
+#endif //BBINTTYPES
